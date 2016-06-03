684da3e2a2ff3a391b42c5a21820013c01dd1641
B15030329 <1550886983@qq.com>
2016-06-03 09:24:55 +0800

    6月3日
    B15030329
    B15030330
    B15030320

--- B150303_学生材料/02含批阅意见的实验报告/第10小组：题目名称/源程序及数据文件/file.h
+++ B150303_学生材料/02含批阅意见的实验报告/第10小组：题目名称/源程序及数据文件/file.h
@@ -1,49 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"hero.h"
-int createFile(Hero hero[])
+int createFile(Hero hero[])       /*建立初始的数据文件*/
 {
 	FILE *fp;
 	int n;
-	if((fp=fopen("d:\\hero.dat", "wb")) == NULL)
+	if((fp=fopen("d:\\hero.dat", "wb")) == NULL)  /*指定好文件名，以写入方式打开*/
 	{
-		printf("can not open file !\n");
-		exit(0);
+		printf("can not open file !\n");     /*若打开失败，输出提示信息*/
+		exit(0);                              /*然后退出*/
 	}
-	printf("input heros\' information:\n");
-	n=readHero(hero,NUM);
-	fwrite(hero,sizeHero,n,fp);
-	fclose(fp);
+	printf("请输入英雄信息：\n");
+	n=readhero(hero,NUM);                /*调用hero.h中的函数读数据*/
+	fwrite(hero,sizehero,n,fp);           /*将刚才读入的所有记录一次性写入文件*/
+	fclose(fp);                              /*关闭文件*/
 	return n;
 }
 
-int readFile(Hero hero[] ) 
+int readFile(Hero hero[] )              /*将文件中的内容读出置于结构体数组hero中*/
 {
 	FILE *fp;
 	int i=0;
-	if((fp=fopen("d:\\hero.dat", "rb")) == NULL)
+	if((fp=fopen("d:\\hero.dat", "rb")) == NULL)  /*以读的方式打开指定文件*/
 	{
-		printf("file does not exist,create it first:\n");
-		return 0;
+		printf("没有找到文件，请先创建:\n");   /*如果打开失败输出提示信息*/
+		return 0;          /*然后返回0*/
 	}
-	fread(&hero[i],sizeHero,1,fp);
-	while(!feof(fp))
-	{
+	fread(&hero[i],sizehero,1,fp);            /*读出第一条记录*/
+	while(!feof(fp))                       /*文件未结束时循环*/
+	{ 
 		i++;
-		fread(&hero[i],sizeHero,1,fp);
+		fread(&hero[i],sizehero,1,fp);        /*再读出下一条记录*/
 	}
-	fclose(fp);
-	return i;
-}
+	fclose(fp);                               /*关闭文件*/
+	return i;                                   /*返回记录条数*/
+} 
 
-void saveFile(Hero hero[],int n)
+void saveFile(Hero hero[],int n)                     /*将结构体数组的内容写入文件*/
 {
 	FILE *fp;
-	if((fp=fopen("d:\\hero.dat","wb"))==NULL)
+	if((fp=fopen("d:\\hero.dat","wb"))==NULL)      /*以写的方式打开指定文件*/
 	{
-		printf("can not open file !\n");
-		exit(0);
+		printf("不能打开文件！\n");                /*如果打开失败，输出提示信息*/
+		exit(0);                                    /*然后退出*/
 	}
-	fwrite(hero,sizeHero,n,fp);
-	fclose(fp);
+	fwrite(hero,sizehero,n,fp);
+	fclose(fp);                                 /*关闭文件*/
 }
--- B150303_学生材料/02含批阅意见的实验报告/第10小组：题目名称/源程序及数据文件/hero.h
+++ B150303_学生材料/02含批阅意见的实验报告/第10小组：题目名称/源程序及数据文件/hero.h
@@ -1,12 +1,12 @@
-#ifndef _HERO
+#ifndef _HERO      /*条件编译，防止重复包含的错误*/
 #define _HERO
 #include <string.h>
-#define NUM 20
-struct Hero
+#define NUM 20     /*定义英雄数常量*/
+struct Hero        /*英雄记录的数据域*/
 {
-	long num;
+	int num;
 	char name [20];
-	char skill [20];
+	char skill [50];
 	char character[20];
 	char partner[20];
 	char rival[20];
@@ -14,17 +14,18 @@ struct Hero
 	int appearance;
 	int rank;
 	double rate;
-	int kill;
-	int death;
+	double kill;
+	double death;
 };
 typedef struct Hero Hero;
-#define sizeHero sizeof(Hero)
-int readHero( Hero hero[],int n);
-void printHero( Hero hero[],int n);                   
-void calcuRank(Hero hero[],int n);                  /*根据胜率来计算英雄名次*/
-int larger(Hero h1,Hero h2,int condition);    
-void sortHero(Hero hero[],int n,int condition);     /*选择法从小到大排序，按condition所规定的条件*/
-int searchHero(Hero hero[],int n,Hero h,int condition,int f[]) ; 
-int insertHero(Hero hero[],int n,Hero h);
-int deleteHero(Hero hero[],int n,Hero h);							
+#define sizehero sizeof(Hero)         /*一个英雄记录所需要的内存空间大小*/
+int readhero( Hero hero[],int n);      /*读入英雄记录值，编号为0或读满规定条数记录时停止*/
+void printhero( Hero *hero,int n);      /*输出所有英雄记录的值*/
+int equal(Hero h1,Hero h2,int condition) ;    /*根据condition条件判断两个Hero类型数据相等否*/
+int larger(Hero h1,Hero h2,int condition);    /*根据condition比较Hero类型数据大小*/
+void sorthero(Hero hero[],int n,int condition);      /*选择法从小到大排序，按condition所规定的条件*/               
+void calcuRank(Hero hero[],int n);                  /*根据胜率来计算英雄名次*/    
+int searchhero(Hero hero[],int n,Hero h,int condition,int f[]) ; /*根据条件找数组中与h相等的各元素*/
+int inserthero(Hero hero[],int n,Hero h);         /*向数组中插入一个元素按编号有序*/
+int deletehero(Hero hero[],int n,Hero h);			/*从数组中删除一个指定编号的元素*/				
 #endif
